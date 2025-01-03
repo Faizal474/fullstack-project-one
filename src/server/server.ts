610e901f1978996cff3cbf66d893f4c6f0d1955a
moteesh-reddy <moteesh@yahoo.com>
2025-01-03 22:04:38 +0530

    <%= content -%>

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,7 +5,9 @@ server.set("view engine", "ejs");
 
 server.use(express.static("dist"));
 server.use("/", (req, res) => {
-    res.render("index");
+    res.render("index", {
+        content: "this is from express",
+    });
 });
 server.listen("8080", "0.0.0.0", () => {
     console.info("Express server listening at http://0.0.0.0:8080");
