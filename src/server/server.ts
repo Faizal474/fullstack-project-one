3bc776458669e3d3c21e44715d0fdb2e0c634db4
moteesh-reddy <moteesh@yahoo.com>
2025-01-03 22:01:35 +0530

    http://localhost:8080/style.css

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,2 +1,7 @@
-import * as express from "express";
-console.log(express);
+import express from "express"
+
+const server = express()
+server.use(express.static("dist"));
+server.listen("8080", "0.0.0.0", () => {
+    console.info("Express server listening at http://0.0.0.0:8080");
+});
