05cf153cf8518b57413c9453ea147dea9033df2f
moteesh-reddy <moteesh@yahoo.com>
2025-01-03 22:02:28 +0530

    npm i ejs

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,9 @@ import express from "express"
 
 const server = express()
 server.use(express.static("dist"));
+server.use("/", (req, res) => {
+    res.send("Hello World!!");
+});
 server.listen("8080", "0.0.0.0", () => {
     console.info("Express server listening at http://0.0.0.0:8080");
 });
