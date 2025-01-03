3476419b01a6ea526cf3c90ff9a8986398c5298b
moteesh-reddy <moteesh@yahoo.com>
2025-01-03 23:38:57 +0530

    demonstrating list display

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
 import { createRoot } from "react-dom/client";
 
 const App = () => {
+    let myrandom = Math.random();
+    console.log(myrandom);
+    const TODOs = ["my task 1", "my task 2", "my task 3", "one more"];
+    
     return (
-        <div>Hello from React</div>
+        <div>
+            <h1>Hello From React </h1>
+            <h2>With JSX </h2>
+            { myrandom > 0.5 && <h2> {myrandom} this is a conditional render </h2>}
+            {
+                TODOs.map((element) => {
+                    return <li>{element}</li>
+                })
+            }
+            
+        </div>
     );
 };
 
