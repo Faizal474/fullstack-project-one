7ea40079ababd2a8cc3ddeeb2dd38dc6b938deda
moteesh-reddy <moteesh@yahoo.com>
2025-01-04 12:00:59 +0530

    serving static files

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,18 @@
-import * as express from "express";
-console.log(express);
+import express from "express";
+
+const server = express();
+
+server.use(express.static("dist"));
+
+server.use("/students", (req, res) => {
+    res.send("This is coming from /students path");
+});
+
+server.use("/", (req, res) => {
+    res.send("This is coming from / path");
+});
+
+server.listen("8080", "0.0.0.0", () => {
+    console.info("Express server listening at http://0.0.0.0:8080");
+});
 
